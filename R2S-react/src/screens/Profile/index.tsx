2ddde2829c5777e0ff27969936d4cf9a37a033e1
levando20194017 <levando194017@gmail.com>
2023-06-03 22:25:08 +0700

    update profile UI

--- R2S-react/src/screens/Profile/index.tsx
+++ R2S-react/src/screens/Profile/index.tsx
@@ -1,10 +1,12 @@
 import { PersonalForm } from "../../components/Personal";
+import { PostForm } from "../../components/Post";
 import './style.css'
 const ProfilePage = () => {
     return (
         <div className="profile-page-container">
             <div className="container">
                 <PersonalForm />
+                <PostForm />
             </div>
         </div>
     );
